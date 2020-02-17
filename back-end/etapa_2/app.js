a00aafd79b30de67d1501a5e3e486d682c12f65a
Victor <felipe.beleza29@gmail.com>
2020-02-17 07:32:29 -0300

    Atualizações finais

--- back-end/etapa_2/app.js
+++ back-end/etapa_2/app.js
@@ -1,17 +1,54 @@
 var request = require('request');
 var priceReduction = require('./modules/priceReduction');
 var mostPopular = require('./modules/mostPopular');
+var findProducts = require('./modules/findStep1')
 var express = require('express');
 var app = express();
 
 app.get('/', function(req, res, next) {
+    res.setHeader('Access-Control-Allow-Origin', '*');
+    var mostPopularProducts = [];
+    var priceReductionProducts = [];
+    var allProducts = [];
     mostPopular().then((result) => {
-        console.log('1')
+        let productsId = JSON.parse(result);
+        for(let i = 0; i < productsId.length; i++){
+            let idString = productsId[i].recommendedProduct.id;   
+            findProducts(idString).then((product) => {
+                let prdct = (JSON.parse(product))
+                mostPopularProducts.push(prdct);
+                if(mostPopularProducts.length == 90){
+                    priceReduction().then((resultR) => {
+                        let productRId = (JSON.parse(resultR));
+                        for (let y = 0; y < productRId.length; y++){
+                            let idStringR = productRId[y].recommendedProduct.id;
+                            findProducts(idStringR).then((productReduct) => {
+                                let redPrdct = JSON.parse(productReduct)
+                                priceReductionProducts.push(redPrdct);
+                                console.log(priceReductionProducts.length)
+                                if(priceReductionProducts.length == 87){
+                                    allProducts.push(mostPopularProducts);
+                                    allProducts.push(priceReductionProducts);
+                                    res.status(200).send(allProducts);
+                                }
+                            }).catch(err => console.log(err));   
+                        }
+                    });
+                }
+            }).catch(error => {
+
+            })
+            
+        }
+        console.log(mostPopularProducts)
+        
+    }).catch(err => {
+        console.log(err)
     });
 
-    priceReduction().then((result) => {
-        console.log('2')
-     });
+    
+
+    
 });
 
 app.listen(8081, function(){
--- front-end/main.js
+++ front-end/main.js
@@ -1,46 +1,25 @@
+var products;
+const url = 'http://localhost:8081/';
+fetch(url).then(function (response) {
+  return response.json().then(function(json) {
+    products = json;
+    for(let i = 0; i < products[1].length; i++){
+        $('.slider-price-reduction').slick('slickAdd','<image src=\"https:' + products[1][i].images[0].path + '\" > <p>' + products[1][i].name + '</p> </div> ');
+    }
+    for(let i = 0; i < products[0].length; i++){
+      $('.slider-mostpopular').slick('slickAdd','<div> <image src=\"https:' + products[0][i].images[0].path + '\" > <p>' + products[0][i].name + '</p> </div> ');
+  }
+  });
+})
+
 $('.slider-mostpopular').slick({
-    dots: true,
     infinite: false,
     speed: 300,
     slidesToShow: 4,
-    slidesToScroll: 4,
-    responsive: [
-      {
-        breakpoint: 1024,
-        settings: {
-          slidesToShow: 3,
-          slidesToScroll: 3,
-          infinite: true,
-          dots: true
-        }
-      },
-      {
-        breakpoint: 600,
-        settings: {
-          slidesToShow: 2,
-          slidesToScroll: 2
-        }
-      },
-      {
-        breakpoint: 480,
-        settings: {
-          slidesToShow: 1,
-          slidesToScroll: 1
-        }
-      }
-      // You can unslick at a given breakpoint now by adding:
-      // settings: "unslick"
-      // instead of a settings object
-    ]
   });
-  fetch('http://localhost:8081/', {mode: 'no-cors'})
-    .then((response) => {
-      if(!response.ok) throw new Error('Erro ao executar a requisição, status ' + response.status)
-      return response.json();
-    })
-    .then((data) => {
-      console.log(data)
-    })
-    .catch((error) => {
-      console.log(error)
-    })
+
+  $('.slider-price-reduction').slick({
+    speed: 300,
+    slidesToShow: 4,
+    slideToScroll: 4
+  });
